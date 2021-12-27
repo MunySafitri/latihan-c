0aed58cd0d376d096cff8d841f709a8e705fa758
Mumun.sf <82895736+munyaja@users.noreply.github.com>
2021-12-27 15:11:38 +0700

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -3,12 +3,12 @@
 int main(){
 	int a, b, c;
 	printf("Inputkan nilai a: ");
-	scanf("%i", &a);
+	scanf("%i", &a);//& artinya memegang nilai untuk di simpan ke varibel
 
 	printf("Inputkan nilai b: ");
 	scanf("%i",&b);
 
 	c=a+b;
-	printf("Hasil a+b: %i", &c);
+	printf("Hasil a+b: %i", c);
 	//ingat tak ada nilai return pada type void
 }
