b5c59723eeec022ec5be1104027296b198f6f143
Mumun.sf <82895736+munyaja@users.noreply.github.com>
2022-01-07 22:12:51 +0700

    Update ukuranArray.c

--- ukuranArray.c
+++ ukuranArray.c
@@ -1,7 +1,25 @@
-int array[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+// int array[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-/* ukuran array dalam byte */
-int size = sizeof(array);
+// /* ukuran array dalam byte */
+// int size = sizeof(array);
 
-/* banyaknya isi array `array` */
-int length = sizeof(array) / sizeof(*array); 
+// /* banyaknya isi array `array` */
+// int length = sizeof(array) / sizeof(*array); 
+
+#include <stdio.h>
+int main()
+{
+	int i, j;
+	char* a[2][3] ={{"aku","dia","anda"},{"kamu","mereka","kami"}};
+  	
+//	int b =sizeof(*a);
+//	printf("%i",b);
+	
+	for(i = 0; i<sizeof(a)/sizeof(*a); i++){//a =24 , *a =12
+		for(j = 0; j<sizeof(a[i])/sizeof(*a[i]);j++){
+			printf("%s\n",a[i][j]);
+		}
+	}
+	printf("Keluar dari Program");
+	return 0;
+}
